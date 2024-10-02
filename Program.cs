8d03b796eaef665edf8ac1a2fe40e921df58f51c
Ismingiz <boburmamashukurov08@gmail.com>
2024-10-02 22:14:27 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿int KeshaAge = 22;
+System.Console.WriteLine("Enter your name  ");
+string name = Console.ReadLine();
+string greeting = $"Hello {name}";
+System.Console.WriteLine(greeting);
+
+System.Console.WriteLine("Enter your age:  ");
+string AgeAsString = Console.ReadLine();
+System.Console.WriteLine("Converting...  ");
+int age = Convert.ToInt32(AgeAsString);
+System.Console.WriteLine($"Successfully converted {age}");
+int ageDifference = age - KeshaAge;
+System.Console.WriteLine($"Sizning yoshingiz va Kescha yoshi o'rtasidagi farq: {ageDifference} yil ");
+
+
+
+
+
+
+
+
+
+
+
+// System.Console.WriteLine("Enter a number with decimal places: ");
+// string numberAsString = Console.ReadLine();
+// System.Console.WriteLine("Converting to double...");
+// double number = Convert.ToDouble(numberAsString);
+// System.Console.WriteLine($"Successfully converted {number}");
+
+// System.Console.WriteLine("Enter your height in meters: ");
+// string heightAsString = Console.ReadLine();
+// System.Console.WriteLine("Converting to double...");
+// double height = Convert.ToDouble(heightAsString);
+// System.Console.WriteLine($"Successfully converted {height} meters");
+
+
